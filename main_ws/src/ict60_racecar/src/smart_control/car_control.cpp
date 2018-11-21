7aca3c50e98eeb8045340ecc7970d6c685ae203a
Viet-Anh Nguyen <vietanh@vietanhdev.com>
2018-11-21 19:54:28 +0700

    turn off obstacle
    Former-commit-id: d6eb27eca8b3c5365ed5c1aea295341ffc741977
    Former-commit-id: 63a0edbcd3597f6852397d7a675d5be0041283a4

--- main_ws/src/ict60_racecar/src/smart_control/car_control.cpp
+++ main_ws/src/ict60_racecar/src/smart_control/car_control.cpp
@@ -220,30 +220,30 @@ void CarControl::driverCar(Road & road, const std::vector<TrafficSign> & traffic
     
 
     //Find the obstacle and adjust obstacle_avoid_coeff
-    for (int i = road.middle_points.size()-5; i >=  10; --i) {
-        int diff = road.middle_points[i+3].x - road.middle_points[i].x;
-        int distance_to_obstacle;
+    // for (int i = road.middle_points.size()-5; i >=  10; --i) {
+    //     int diff = road.middle_points[i+3].x - road.middle_points[i].x;
+    //     int distance_to_obstacle;
 
-        if (abs(diff) > 5 and abs(diff) < 10) {
-            distance_to_obstacle = Road::road_center_line_x - road.middle_points[i].y;
+    //     if (abs(diff) > 8 and abs(diff) < 20) {
+    //         distance_to_obstacle = Road::road_center_line_x - road.middle_points[i].y;
             
-            if (debug_flag) {
-                std::cout << "OBSTACLE DISTANCE: " << distance_to_obstacle << std::endl;
-            }
+    //         if (debug_flag) {
+    //             std::cout << "OBSTACLE DISTANCE: " << distance_to_obstacle << std::endl;
+    //         }
 
-            obstacle_avoiding_time_point = std::chrono::high_resolution_clock::now();
+    //         obstacle_avoiding_time_point = std::chrono::high_resolution_clock::now();
         
-            if (diff > 0) {
-                obstacle_avoid_coeff = -5;
-                break;
-            } else if (diff < 0) {
-                obstacle_avoid_coeff = +5;
-                break;
-            }
+    //         if (diff > 0) {
+    //             obstacle_avoid_coeff = -10;
+    //             break;
+    //         } else if (diff < 0) {
+    //             obstacle_avoid_coeff = +10;
+    //             break;
+    //         }
         
-        }
+    //     }
 
-    }
+    // }
     
 
     angle_data += obstacle_avoid_coeff + turning_coeff;
